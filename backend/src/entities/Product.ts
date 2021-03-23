aae03a9c62d9d1858d6187c6ff47ef29440fd5d9
wallace.jeronimo <wallace.jeronimo@aec.com.br>
2021-03-23 18:13:30 -0300

    Configuracao entidade produto

--- backend/src/entities/Product.ts
+++
@@ -1,9 +0,0 @@
-export class Product{
-    id: number;
-    name: string;
-    description: number;
-    quantity: number;
-    price: number;
-    active: boolean;
-    created_at: Date;
-}
---
+++ backend/src/entity/Product.ts
@@ -0,0 +1,26 @@
+import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+
+@Entity()
+export class Product{
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    quantity: number;
+
+    @Column()
+    price: number;
+
+    @Column()
+    active: boolean;
+
+    @Column()
+    created_at: Date;
+}
